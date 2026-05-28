c74b7371deb04778e5ce7a246f06a0f49198a0ef
cloane-online <cloane.signature@gmail.com>
2026-05-28 14:39:22 +0200

    Delete App.jsx

--- App.jsx
+++
@@ -1,6 +0,0 @@
-import CommandesB2B from "./CommandesB2B.jsx";
-
-export default function App() {
-  // Pas de prop onBack : l'appli est autonome (le bouton "Menu" est masqué).
-  return <CommandesB2B />;
-}
