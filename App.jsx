a2775c503f645bd40b0dbb0e4dbf3c19df9480e1
cloane-online <cloane.signature@gmail.com>
2026-05-28 14:23:19 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,6 @@
+import CommandesB2B from "./CommandesB2B.jsx";
+
+export default function App() {
+  // Pas de prop onBack : l'appli est autonome (le bouton "Menu" est masqué).
+  return <CommandesB2B />;
+}
